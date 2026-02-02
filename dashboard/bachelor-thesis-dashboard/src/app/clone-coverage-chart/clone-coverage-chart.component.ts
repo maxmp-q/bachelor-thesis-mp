3159be9eba06903c6a6cacdecb18f24fb4630baf
maxmp-q <maxmpjp@gmail.com>
2026-02-02 23:19:22 +0100

    add more charts in angular

--- dashboard/bachelor-thesis-dashboard/src/app/clone-coverage-chart/clone-coverage-chart.component.ts
+++ dashboard/bachelor-thesis-dashboard/src/app/clone-coverage-chart/clone-coverage-chart.component.ts
@@ -1,10 +1,11 @@
 import {Component, AfterViewInit, OnDestroy, signal, computed, effect} from '@angular/core';
 import { Chart, ChartConfiguration, ChartType, registerables } from 'chart.js';
+import {BoxPlotController, BoxAndWiskers} from '@sgratzl/chartjs-chart-boxplot';
 import {AnalyzedData} from '../../../shared/interface/data-point';
 import {DataHelper} from '../../../shared/data-helper';
 
 // Register Chart.js components globally
-Chart.register(...registerables);
+Chart.register(...registerables, BoxPlotController, BoxAndWiskers);
 
 interface ValueMap {
   value: number;
@@ -26,7 +27,7 @@ export class CloneCoverageChart implements AfterViewInit, OnDestroy {
   dataPoints = signal<Record<string, AnalyzedData>>(DataHelper.getData);
 
   maxLOC = signal<number>(100000);
-  cloneScatterConfig = computed<ChartConfiguration<'scatter'>>(() => {
+  cloneLOCScatterConfig = computed<ChartConfiguration<'scatter'>>(() => {
     const dataPoints = this.dataPoints();
     const maxLOC = this.maxLOC();
 
@@ -34,16 +35,16 @@ export class CloneCoverageChart implements AfterViewInit, OnDestroy {
     const nonSciCloneCoverage: ValueMap[] = [];
 
     Object.values(dataPoints).forEach(entry => {
-      if(entry.field === "nonSci"){
-        if(entry.LOC < maxLOC) nonSciCloneCoverage.push({count: entry.LOC, value: entry.clone_coverage});
-      } else {
-        if(entry.LOC < maxLOC) sciCloneCoverage.push({count: entry.LOC, value: entry.clone_coverage});
+      const LOC = entry.LOC;
+      if(entry.LOC < maxLOC){
+        entry.field === "nonSci" ?
+          nonSciCloneCoverage.push({count: LOC, value: entry.clone_coverage}) :
+          sciCloneCoverage.push({count: entry.LOC, value: entry.clone_coverage});
       }
     });
 
     const researchPoints = sciCloneCoverage.map((entry) => ({ x: entry.count, y: entry.value }));
     const businessPoints = nonSciCloneCoverage.map((entry) => ({ x: entry.count, y: entry.value }));
-
 
     const config: ChartConfiguration<'scatter'> = {
       type: 'scatter',
@@ -90,15 +91,171 @@ export class CloneCoverageChart implements AfterViewInit, OnDestroy {
     return config;
   });
 
-  BusinessVsResearchByLang = signal<Chart | null>(null);
-  AverageCloneCoverageSci = signal<Chart | null>(null);
-  AverageCloneCoverageNonSci = signal<Chart | null>(null);
-  CloneCoverageScatter= signal<Chart | null>(null);
+  maxAuthors = signal<number>(80);
+  cloneAuthorScatterConfig = computed<ChartConfiguration<'scatter'>>(() => {
+    const dataPoints = this.dataPoints();
+
+    const sciCloneCoverage: ValueMap[] = [];
+    const nonSciCloneCoverage: ValueMap[] = [];
+
+    Object.values(dataPoints).forEach(entry => {
+      const numAuthors = Number(entry.authors);
+      if(numAuthors < this.maxAuthors()){
+        entry.field === "nonSci" ?
+          nonSciCloneCoverage.push({count: numAuthors, value: entry.clone_coverage}) :
+          sciCloneCoverage.push({count: numAuthors, value: entry.clone_coverage});
+      }
+    });
+
+    const researchPoints = sciCloneCoverage.map((entry) => ({ x: entry.count, y: entry.value }));
+    const businessPoints = nonSciCloneCoverage.map((entry) => ({ x: entry.count, y: entry.value }));
+
+
+    const config: ChartConfiguration<'scatter'> = {
+      type: 'scatter',
+      data: {
+        datasets: [
+          {
+            label: 'Research',
+            data: researchPoints,
+            backgroundColor: 'rgba(54, 162, 235, 0.8)',
+            pointRadius: 5
+          },
+          {
+            label: 'Business',
+            data: businessPoints,
+            backgroundColor: 'rgba(255, 99, 132, 0.8)',
+            pointRadius: 5
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            type: 'linear',
+            title: {
+              display: true,
+              text: 'Authors'
+            }
+          },
+          y: {
+            title: {
+              display: true,
+              text: 'Clone Coverage'
+            }
+          }
+        },
+        plugins: {
+          legend: {
+            position: 'top'
+          }
+        }
+      }
+    };
+    return config;
+  });
+
+  maxForks = signal<number>(80);
+  cloneForksScatterConfig = computed<ChartConfiguration<'scatter'>>(() => {
+    const dataPoints = this.dataPoints();
+
+    const sciCloneCoverage: ValueMap[] = [];
+    const nonSciCloneCoverage: ValueMap[] = [];
+
+    Object.values(dataPoints).forEach(entry => {
+      const numForks = Number(entry.forks);
+      if(numForks < this.maxForks()){
+        entry.field === "nonSci" ?
+          nonSciCloneCoverage.push({count: numForks, value: entry.clone_coverage}) :
+          sciCloneCoverage.push({count: numForks, value: entry.clone_coverage});
+      }
+    });
+
+    const researchPoints = sciCloneCoverage.map((entry) => ({ x: entry.count, y: entry.value }));
+    const businessPoints = nonSciCloneCoverage.map((entry) => ({ x: entry.count, y: entry.value }));
+
+
+    const config: ChartConfiguration<'scatter'> = {
+      type: 'scatter',
+      data: {
+        datasets: [
+          {
+            label: 'Research',
+            data: researchPoints,
+            backgroundColor: 'rgba(54, 162, 235, 0.8)',
+            pointRadius: 5
+          },
+          {
+            label: 'Business',
+            data: businessPoints,
+            backgroundColor: 'rgba(255, 99, 132, 0.8)',
+            pointRadius: 5
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            type: 'linear',
+            title: {
+              display: true,
+              text: 'Forks'
+            }
+          },
+          y: {
+            title: {
+              display: true,
+              text: 'Clone Coverage'
+            }
+          }
+        },
+        plugins: {
+          legend: {
+            position: 'top'
+          }
+        }
+      }
+    };
+    return config;
+  });
+
+  //Reactive Plots
+  CloneCoverageLOCScatter= signal<Chart | null>(null);
+  CloneCoverageAuthorScatter= signal<Chart | null>(null);
+  CloneCoverageForksScatter= signal<Chart | null>(null);
+
+
+  allNonReactivePlots = signal<Chart[]>([]);
+
 
   constructor() {
     effect(() => {
-      const config = this.cloneScatterConfig();
-      const chart = this.CloneCoverageScatter();
+      const config = this.cloneLOCScatterConfig();
+      const chart = this.CloneCoverageLOCScatter();
+
+      if (!chart) return;
+      if (config.options) chart.options = config.options;
+
+      chart.data.datasets = config.data!.datasets!;
+
+      chart.update();
+    });
+    effect(() => {
+      const config = this.cloneAuthorScatterConfig();
+      const chart = this.CloneCoverageAuthorScatter();
+
+      if (!chart) return;
+      if (config.options) chart.options = config.options;
+
+      chart.data.datasets = config.data!.datasets!;
+
+      chart.update();
+    });
+    effect(() => {
+      const config = this.cloneForksScatterConfig();
+      const chart = this.CloneCoverageForksScatter();
 
       if (!chart) return;
       if (config.options) chart.options = config.options;
@@ -111,8 +268,12 @@ export class CloneCoverageChart implements AfterViewInit, OnDestroy {
 
   ngAfterViewInit(): void {
     this.createCloneByLang();
+    this.createCloneByField();
     this.createAverageClone();
-    this.createCloneScatter();
+    this.createCloneLOCScatter();
+    this.createCloneAuthorScatter();
+    this.createCloneForksScatter();
+    this.createCloneBoxplot();
   }
 
   /**
@@ -194,7 +355,70 @@ export class CloneCoverageChart implements AfterViewInit, OnDestroy {
 
     const canvas = document.getElementById('BusinessVsResearchByLang') as HTMLCanvasElement;
     if (canvas) {
-      this.BusinessVsResearchByLang.set(new Chart(canvas, config));
+      this.allNonReactivePlots.update(value => [...value, new Chart(canvas, config)]);
+    }
+  }
+
+  /**
+   * Creates a bar chart where the average
+   * clone coverage is displayed by different
+   * fields.
+   * @private
+   */
+  private createCloneByField(): void {
+    const dataPoints = this.dataPoints();
+    const cloneCoverages: Record<string, ValueMap> = {};
+
+    Object.values(dataPoints).forEach(({field, clone_coverage }) => {
+      cloneCoverages[field] ??= {count: 0, value: 0};
+
+      const current = cloneCoverages[field];
+
+      cloneCoverages[field] = {
+        value: (current?.value ?? 0) + clone_coverage,
+        count: (current?.count ?? 0) + 1
+      };
+    });
+
+    const labels: string[] = [];
+    const data: number[] = [];
+
+    Object.entries(cloneCoverages).forEach(([field, entry]) => {
+      const avg = entry ? entry.value / entry.count : 0;
+
+      labels.push(`${field} (count: ${entry?.count ?? 0})`);
+      data.push(avg);
+    });
+
+    const config: ChartConfiguration = {
+      type: 'bar' as ChartType,
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Field Average',
+            data: data,
+            backgroundColor: 'rgba(54, 162, 235, 0.6)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: { position: 'top' },
+          title: {
+            display: true,
+            text: 'Different Fields in Clone Coverage'
+          }
+        }
+      }
+    };
+
+    const canvas = document.getElementById('BusinessVsResearchByField') as HTMLCanvasElement;
+    if (canvas) {
+      this.allNonReactivePlots.update(value => [...value, new Chart(canvas, config)]);
     }
   }
 
@@ -252,30 +476,94 @@ export class CloneCoverageChart implements AfterViewInit, OnDestroy {
 
     const canvas1 = document.getElementById('AverageCloneCoverageSci') as HTMLCanvasElement;
     const canvas2 = document.getElementById('AverageCloneCoverageNonSci') as HTMLCanvasElement;
-
-    if (canvas1) this.AverageCloneCoverageSci.set(new Chart(canvas1, config(true)));
-    if (canvas2) this.AverageCloneCoverageNonSci.set(new Chart(canvas2, config(false)));
+    this.allNonReactivePlots.update(value => [...value, new Chart(canvas1, config(true))]);
+    this.allNonReactivePlots.update(value => [...value, new Chart(canvas2, config(false))]);
   }
 
   /**
    * Creates a scatter plot where the dots are sorted by LOC and Clone Coverage!
    * @private
    */
-  private createCloneScatter(): void {
-    const canvas = document.getElementById('CloneCoverageScatter') as HTMLCanvasElement;
+  private createCloneLOCScatter(): void {
+    const canvas = document.getElementById('CloneCoverageLOCScatter') as HTMLCanvasElement;
     if (canvas) {
-      this.CloneCoverageScatter.set(new Chart(canvas, this.cloneScatterConfig()));
+      this.CloneCoverageLOCScatter.set(new Chart(canvas, this.cloneLOCScatterConfig()));
+    }
+  }
+
+  /**
+   * Creates a scatter plot where the dots are sorted by Author and Clone Coverage!
+   * @private
+   */
+  private createCloneAuthorScatter(): void {
+    const canvas = document.getElementById('CloneCoverageAuthorScatter') as HTMLCanvasElement;
+    if (canvas) {
+      this.CloneCoverageAuthorScatter.set(new Chart(canvas, this.cloneAuthorScatterConfig()));
+    }
+  }
+
+  /**
+   * Creates a scatter plot where the dots are sorted by Forks and Clone Coverage!
+   * @private
+   */
+  private createCloneForksScatter(): void {
+    const canvas = document.getElementById('CloneCoverageForksScatter') as HTMLCanvasElement;
+    if (canvas) {
+      this.CloneCoverageForksScatter.set(new Chart(canvas, this.cloneForksScatterConfig()));
+    }
+  }
+
+  /**
+   * Creates a boxplot where the dots are sorted by Author and Clone Coverage!
+   * @private
+   */
+  private createCloneBoxplot(): void {
+    const dataPoints = this.dataPoints();
+
+    const sciCloneCoverage: number[] = [];
+    const nonSciCloneCoverage: number[] = [];
+
+    Object.values(dataPoints).forEach(entry => {
+      entry.field === "nonSci" ?
+          nonSciCloneCoverage.push(entry.clone_coverage) :
+          sciCloneCoverage.push(entry.clone_coverage);
+    });
+
+    sciCloneCoverage.sort((a, b) => a < b ? a : b);
+    nonSciCloneCoverage.sort((a, b) => a < b ? a : b);
+
+    const config: ChartConfiguration<'boxplot'> = {
+      type: 'boxplot',
+      data: {
+        labels: ['Research', 'Business', 'Average'],
+        datasets: [
+          {
+            label: 'Clone Coverage',
+            data: [
+              sciCloneCoverage,     // Boxplot 1
+              nonSciCloneCoverage,   // Boxplot 2
+              [...sciCloneCoverage, ...nonSciCloneCoverage] //Boxplot 3
+            ],
+            backgroundColor: ['rgba(255, 99, 132, 0.4)','rgba(255, 99, 132, 0.4)', 'rgba(54, 162, 235, 0.8)'],
+          }
+        ]
+      }
+    };
+
+
+    const canvas = document.getElementById('CloneCoverageBoxPlot') as HTMLCanvasElement;
+    if (canvas) {
+      this.allNonReactivePlots.update(value => [...value, new Chart(canvas, config)]);
     }
   }
 
   ngOnDestroy(): void {
-    const chart1 = this.BusinessVsResearchByLang();
-    const chart2 = this.AverageCloneCoverageSci();
-    const chart3 = this.AverageCloneCoverageNonSci();
-    const chart4 = this.CloneCoverageScatter();
+    const nonReactivCharts = this.allNonReactivePlots();
+    const chart1 = this.CloneCoverageLOCScatter();
+    const chart2 = this.CloneCoverageAuthorScatter();
+
+    nonReactivCharts.forEach(chart => chart.destroy());
     if (chart1) chart1.destroy();
     if (chart2) chart2.destroy();
-    if (chart3) chart3.destroy();
-    if (chart4) chart4.destroy();
   }
 }
--- src/teamscale/project_deleter.py
+++ src/teamscale/project_deleter.py
@@ -10,9 +10,13 @@ with open('../data/analyzed_data.json', 'r') as data_file:
 
     print(f"Wir haben {data.__len__()}  Einträgen zum löschen!")
 
-    for k in data:
-        print(f"wir löschen gerade: {k}")
-        DELETE_API = f"{BASE_URL}api/v2025.2/projects/{k}"
+    last_50_values = list(data.values())[-50:]
+
+    print(last_50_values)
+
+    for k in last_50_values:
+        print(f"wir löschen gerade: {k['name']}")
+        DELETE_API = f"{BASE_URL}api/v2025.2/projects/{k['name']}"
         print(DELETE_API)
         responses = requests.delete(DELETE_API, auth=(constants.USERNAME, constants.ACCESS_KEY), verify=constants.CERTIFICATE)
         print(responses.status_code)
